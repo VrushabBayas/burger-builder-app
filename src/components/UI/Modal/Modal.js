1262ad1fe9827535ff2a32694a0a808cf795353c
VrushabBayas <rushab.bayas@gmail.com>
2018-10-21 15:08:07 +0530

    basic level burger builder app in reactjs.

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+
+import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Modal extends PureComponent {
+	render() {
+		return (
+			<Aux>
+				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+						opacity: this.props.show ? '1' : '0'
+					}}
+				>
+					{this.props.children}
+				</div>
+			</Aux>
+		);
+	}
+}
+
+export default Modal;
